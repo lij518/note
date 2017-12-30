eb5f3d9d6b8fb2e22cd5041dc8954643dea56df5
李军 <lijun@lijundeMacBook-Pro.local>
2017-12-30 21:53:11 +0800

    c语言入门

---
+++ hello.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+	int num=10;int num1=1;
+	printf("名称：德玛西亚之力•盖伦\n");
+	printf("伤害：57.88（+4.5）\t攻击距离：172\n");
+	printf("护甲：27.536（+3.0）\t魔    抗：32.1（+1.29）\n");
+	printf("生命：616.28（+84.25）\t生命回复：0.0（+0.0）\n");
+	printf("法力：0.0（+0.0）\t法力回复：0.0（+0.0）\n");
+	printf("输出%d%d\n",num,num1);
+	return 0;
+}
