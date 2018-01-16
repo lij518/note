fa32861cb65b9d8c9c5ace5d11134d53f334827a
李军 <lijun@aca80168.ipt.aol.com>
2018-01-16 16:55:41 +0800

    c语言入门学习

---
+++ study/study/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  study
+//
+//  Created by 李军 on 2018/1/16.
+//  Copyright © 2018年 李军. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    system("color bf");
+    return 0;
+}
